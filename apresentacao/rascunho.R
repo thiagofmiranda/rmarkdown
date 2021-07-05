dc862e8de498d90ff9a6f36d26a584eee5d49af4
Thiago F. Miranda <33705632+thiagofmiranda@users.noreply.github.com>
2021-07-05 16:40:39 -0300

    apresentação

---
+++ apresentacao/rascunho.R
@@ -0,0 +1,25 @@
+
+
+rmarkdown::render("exemplos/5 - formatos de saida/formatos de saida.Rmd",output_dir = "exemplos/5 - formatos de saida/", output_format = 'all')
+
+
+
+
+# Para construção de documentos LaTeX usaremos o TinyTex:
+install.packages('tinytex')
+tinytex::install_tinytex()  # instalar o TinyTeX
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
